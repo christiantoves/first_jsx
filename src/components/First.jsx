ce4d9da9d0c788c1904c5cc9be05226dc5c8f924
Christian Toves <christiano.toves@gmail.com>
2021-07-08 12:03:07 -0700

    draft

---
+++ src/components/First.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from "react" 
+
+
+class First extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Dojo!</h1>
+                <p>Things I Need To Do:</p>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Run a Marathon</li>
+                    <li>Get a Girlfriend</li>
+                    <li>Get Married</li>
+                    <li>Not in this Order</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+
+export default First
